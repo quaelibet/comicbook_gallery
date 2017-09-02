86f2c1e096efed883ff7c0239a12e88900909bc4
quaelibet <quaelibet@gmail.com>
2017-09-02 12:16:13 +0200

    ComicBooks Detail page

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,8 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            return View();
+
+            //if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            //{
+            //    return Redirect("/");
+            //    //return new RedirectResult("/");
+            //}
+
+            //return new ContentResult()
+            //{
+            //    Content = "Hello"
+            //};
+        }
     }
 }
